1ac17075ea62ec8a4a71d16686201338aa24fb93
TimlerTheTerp <165680770+TimlerTheTerp@users.noreply.github.com>
2025-04-19 21:41:21 -0400

    Update A2.js

--- A2.js
+++ A2.js
@@ -1,12 +1,21 @@
+/*REQUIRED BELOW */
 if (annyang) {
     // Let's define a command.
     const commands = {
-      'hello': () => { alert('Hello world!'); }
-    };
+      'hello': () => { 
+            alert('Hello world!'); 
+            //change the background color
+            document.body.style.backgroundColor = "white";
+        },
+        'Stocks': () => {
+            window.location.href = "Stocks.html";
+        },
 
-    //change the background color
-    
-    document.body.style.backgroundColor = "white";
+        'Dogs': () => {
+            window.location.href = "Stocks.html";
+        }
+        }
+
 
   
     // Add our commands to annyang
@@ -15,3 +24,35 @@ if (annyang) {
     // Start listening.
     annyang.start();
   }
+/*REQUIRED ABOVE */
+
+function waiting() {
+    let wait = document.getElementById("QuotePlacer")
+    wait.innerHTML = "Loading....."
+
+    /*Timer*/
+    setTimeout(function() {
+        /*using note would make sense because it's part of loadfunction */
+        wait.style.display = ""
+    }, 10000);
+}
+
+/*This Function is responsible for getting the API*/
+function getQuote() {
+    let getAPI ="https://zenquotes.io/api/quotes/";
+}
+
+async function GenerateQuote() {
+   waiting();
+   
+    /*Setting Our Variables */
+    let genQuote = await getQuote()
+    let placer = document.getElementById("QuotePlacer")
+    let items = response.json()
+
+    /*Random Generator
+    
+    /*Setting Up whatever is affrifrated with the quote */
+    let author =
+    let 
+}

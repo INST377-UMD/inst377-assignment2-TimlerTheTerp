5b8f53c94a02041b86ac8f24e442b6d48e4d7d32
TimlerTheTerp <165680770+TimlerTheTerp@users.noreply.github.com>
2025-04-23 18:13:35 -0400

    Update A2.js

--- A2.js
+++ A2.js
@@ -1,60 +1,66 @@
 /*REQUIRED BELOW */
 function Annyang() {
-  let btn = document.querySelector('.VoiceButton');
-if (annyang) {
-    // Let's define a command.
-    const commands = {
-      'hello': () => { 
-            alert('Hello world!'); 
-            //change the background color
-            document.body.style.backgroundColor = "white";
-        },
-        'Stocks': () => {
-            window.location.href = "Stocks.html";
-        },
-
-        'Dogs': () => {
-            window.location.href = "Stocks.html";
-        }
-        }
-
-
+    let btn = document.querySelector('.VoiceButton');
+  if (annyang) {
+      // Let's define a command.
+      const commands = {
+        'hello': () => { 
+              alert('Hello world!'); 
+              //change the background color
+              document.body.style.backgroundColor = "white";
+          },
+          'Stocks': () => {
+              window.location.href = "Stocks.html";
+          },
   
-    // Add our commands to annyang
-    annyang.addCommands(commands);
+          'Dogs': () => {
+              window.location.href = "Stocks.html";
+          }
+          }
   
-    // Start listening.
-    annyang.start();
-  }
+  
+    
+      // Add our commands to annyang
+      annyang.addCommands(commands);
+    
+      // Start listening.
+      annyang.start();
+    }
+}
 /*REQUIRED ABOVE */
 
 function waiting() {
-    let wait = document.getElementById("QuotePlacer")
-    wait.innerHTML = "Loading....."
+    let wait = document.getElementById("Quote");
+    wait.innerHTML = "Loading.....";
 
-    /*Timer*/
-    setTimeout(function() {
-        /*using note would make sense because it's part of loadfunction */
-        wait.style.display = ""
-    }, 10000);
 }
 
 /*This Function is responsible for getting the API*/
 function getQuote() {
-    let getAPI ="https://zenquotes.io/api/quotes/";
+    return fetch("https://zenquotes.io/api/random").then((result) =>
+        result.json()
+    );
 }
 
 async function GenerateQuote() {
    waiting();
    
     /*Setting Our Variables */
-    let genQuote = await getQuote()
-    let placer = document.getElementById("QuotePlacer")
-    let items = response.json()
-
-    /*Random Generator
+    let genQuote = await getQuote();
+    let placer = document.getElementById("Quote");
+    let writer = document.getElementById("Author");
     
     /*Setting Up whatever is affrifrated with the quote */
-    let author =
-    let 
+    let quote = genQuote[0].q;
+    let author = genQuote[0].a;
+
+    placer.innerHTML = `${quote}`;
+    writer.innerHTML = `- ${author}`;
 }
+
+/*Timer*/
+setTimeout(function() {
+    wait.style.display = "";
+}, 5000);
+
+window.onload = GenerateQuote;

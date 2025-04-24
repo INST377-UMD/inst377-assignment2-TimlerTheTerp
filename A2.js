4b6155032673608bed7d4608ec2bbb7b4e34ecc7
TimlerTheTerp <165680770+TimlerTheTerp@users.noreply.github.com>
2025-04-24 08:11:56 -0400

    Update A2.js

--- A2.js
+++ A2.js
@@ -1,6 +1,5 @@
-/*REQUIRED BELOW */
+*REQUIRED BELOW */
 function Annyang() {
-    let btn = document.querySelector('.VoiceButton');
   if (annyang) {
       // Let's define a command.
       const commands = {
@@ -12,13 +11,13 @@ function Annyang() {
           'Stocks': () => {
               window.location.href = "Stocks.html";
           },
-  
+
           'Dogs': () => {
               window.location.href = "Stocks.html";
           }
           }
-  
-  
+
+
     
       // Add our commands to annyang
       annyang.addCommands(commands);
@@ -26,46 +25,45 @@ function Annyang() {
       // Start listening.
       annyang.start();
     }
-}
+  }
 /*REQUIRED ABOVE */
 
 function waiting() {
-    let wait = document.getElementById("Quote");
-    wait.innerHTML = "Loading.....";
+  let wait = document.getElementById("Quote");
+  wait.innerHTML = "Loading.....";
 
-    /*Timer*/
-    setTimeout(function() {
-        wait.style.display = "none";
-    }, 5000);
+
+ 
 }
 
 /*This Function is responsible for getting the API*/
 function getQuote() {
-    return "https://api.allorigins.win/raw?url=https://zenquotes.io/api/quotes/";
+  //Since this API has restrictions, I used extensive research to figure out why this isn't responding correctly
+  return "https://api.allorigins.win/raw?url=https://zenquotes.io/api/random&" + Date.now();
 }
 
-async function GenerateQuote() {
-    waiting();
-   
-    
-     /*Setting Our Variables */
-    const response = await fetch(getQuote());
-    var data = await response.json();
+/*Main Function For Quotes */
+function GenerateQuote() {
+  waiting();
+ 
+  
+  setTimeout(async function () {
+    /*Setting Our Variables */
+    let response = await fetch(getQuote());
+    let data = await response.json();
     let placer = document.getElementById("Quote");
     let writer = document.getElementById("Author");
 
-    /*Random Generator*/
-    tab = Math.floor(Math.random() * 51);
     
     /*Setting Up whatever is affrifrated with the quote */
-    let Firstquote = data[tab];
+    let Firstquote = data[0];
     let quote = Firstquote.q;
     let author = Firstquote.a;
 
     placer.innerHTML = `${quote}`;
     writer.innerHTML = `- ${author}`;
+}, 5000);
+
 }
-
-
 
 window.onload = GenerateQuote;
